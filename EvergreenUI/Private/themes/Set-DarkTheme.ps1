e73c69d030f6bf1654ac57730d2b8c40a61d1517
Aaron Parker <aaron@stealthpuppy.com>
2026-04-03 20:28:08 +1100

    Revert "Add custom window chrome and DWM border color"
    This reverts commit d4fb5bab69e6ebbf43c69ad93cf015ae2c6368e6.

--- EvergreenUI/Private/themes/Set-DarkTheme.ps1
+++ EvergreenUI/Private/themes/Set-DarkTheme.ps1
@@ -70,5 +70,5 @@ function Set-DarkTheme {
 
     # Colour the native OS title bar to match AccentBrush #4DB8AD (R=77 G=184 B=173)
     # COLORREF byte order is 0x00BBGGRR → 0x00ADB84D
-    Set-DwmTitleBarColor -Window $Window -CaptionColorRef 0x00ADB84D -BorderColorRef 0x00ADB84D -UseDarkMode $true
+    Set-DwmTitleBarColor -Window $Window -CaptionColorRef 0x00ADB84D -UseDarkMode $true
 }
